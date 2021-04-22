550fa540f00b80dd1dcaa06a4d8d781596a83477
Michael Joyce <michael.joyce@illiniarts.com>
2021-04-21 19:17:38 -0500

    remove cur from sql_runner and add select * function

--- test/sql_runner.py
+++ test/sql_runner.py
@@ -2,7 +2,7 @@ from convert_var import convert_var
 from pytest import fixture
 from db import conn
 
-def func(function_name, cur, *args):
+def func(function_name, *args):
     var_list = [convert_var(var) for var in args]
     sql = "SELECT " + function_name + "(" + ", ".join(["{}"]*len(var_list)).format(*var_list) + ");"
     print(sql)
@@ -10,12 +10,19 @@ def func(function_name, cur, *args):
     cur.execute(sql)
     return cur.fetchone()
 
-def select_one(table_name, cur, *field_list, condition = 'true'):
+def select_one(table_name, *field_list, condition = 'true'):
     sql = "SELECT " + ", ".join(["{}"]*len(field_list)).format(*field_list)+" FROM {} WHERE {}".format(table_name, condition)
     print(sql)
     cur = conn.cursor()
     cur.execute(sql)
     return cur.fetchone()
+
+def select_all(table_name, condition = 'true'):
+    sql = "SELECT * FROM {} WHERE {}".format(table_name, condition)
+    print(sql)
+    cur = conn.cursor()
+    cur.execute(sql)
+    return cur.fetchall()
 
 def func_row_type(function_name, dict):
     var_list = [convert_var(value) for key, value in dict.items()]
