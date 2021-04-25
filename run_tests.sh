3b87c5b3d6af6647c58c9b8a86753adbedacc73f
Michael Joyce <michael.joyce@illiniarts.com>
2021-04-25 10:51:26 -0500

    added comments to the shell script

--- run_tests.sh
+++ run_tests.sh
@@ -33,21 +33,21 @@ wait_on_db_connection () {
     until docker exec $DB_CONTAINER pg_isready ; do sleep 3 ; done
 }
 
+# check if the first parameter = "rebuild"
+# if so, rebuild all the containers
+# else, check for the images and build them as necessary
 if [ "$1" == "rebuild" ]
-    then # rebuild the images
+    then
     shift # throw away 1st parameter any others will be passed to pytest
     echo "Rebuilding the container images..."
     rebuild_db_container
     rebuild_test_container
 else
-    # build the DB_IMAGE if it doesn't exist
     OUTPUT=$(docker image ls -q $DB_IMAGE)
     if [ ! $OUTPUT ]
         then
         rebuild_db_container
     fi
-
-    # build the TEST_IMAGE if it doesn't exist
     OUTPUT=$(docker image ls -q $TEST_IMAGE)
     if [ ! $OUTPUT ]
         then
@@ -62,7 +62,10 @@ docker run -d --rm --name $DB_CONTAINER --publish 5432:5432 $DB_IMAGE
 # wait until the DB_CONTAINER is accepting connections
 wait_on_db_connection
 
-# check for any parameters to the pytest command
+# start the pytest container
+# we don't name it or need to stop it, it only runs the command and then stops
+# if no parameters, just run the container
+# if parameters, override the image command with a new one containing the parameters
 if [ $# -eq 0 ]
     then
     docker run --rm --mount $TEST_MOUNT $TEST_IMAGE
@@ -70,5 +73,6 @@ else
     docker run --rm --mount $TEST_MOUNT $TEST_IMAGE pytest "$@"
 fi
 
+# stop the db container
 echo "Stopping database"
 docker stop ephemeral-test-db
