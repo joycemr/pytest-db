26645cf4758ddd9a1b6d834783eb71dcc9991bbe
Michael Joyce <michael.joyce@illiniarts.com>
2021-04-24 20:41:34 -0500

    added comment for db url's

--- test/db.py
+++ test/db.py
@@ -6,7 +6,10 @@ class NoDatabaseUrlSet(Exception):
     pass
 
 # for testing
+# local
 # export SQL_UNIT_TEST_TEMPLATE_DB_URL=postgresql://postgres:postgres@localhost:5432/postgres
+# container
+# export SQL_UNIT_TEST_TEMPLATE_DB_URL=postgresql://postgres:postgres@host.docker.internal:5432/postgres
 DATABASE_URL = os.getenv("SQL_UNIT_TEST_TEMPLATE_DB_URL")
 if DATABASE_URL == None:
     raise NoDatabaseUrlSet('The environment variable SQL_UNIT_TEST_TEMPLATE_DB_URL needs to be set to point the the test database')
