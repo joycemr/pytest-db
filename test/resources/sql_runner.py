b42fa644e5f5b6c4b8c6155a478dd83775263813
Michael Joyce <michael.joyce@illiniarts.com>
2021-04-29 20:28:11 -0500

    savepoint

--- test/resources/sql_runner.py
+++ test/resources/sql_runner.py
@@ -17,6 +17,7 @@ def run_sql_with_params(sql, params):
     with pytest.conn.cursor(cursor_factory=NamedTupleCursor) as cur:
         try:
             cur.execute(sql, params)
+            return cur.fetchall()
         except:
             pytest.conn.rollback()
 
@@ -26,7 +27,7 @@ def select(table_name, *field_list, condition = 'true'):
     return run_sql(sql)
 
 def insert(table_name, data_dict):
-    sql = "INSERT INTO " + table_name + "(" + get_field_list(data_dict) + ") VALUES (" + get_param_placeholders(data_dict) + ")"
+    sql = "INSERT INTO " + table_name + "(" + get_field_list(data_dict) + ") VALUES (" + get_param_placeholders(data_dict) + ") RETURNING *"
     return run_sql_with_params(sql, data_dict)
 
 def delete(table_name, condition = 'true'):
@@ -35,10 +36,10 @@ def delete(table_name, condition = 'true'):
 
 def function(function_name, *args):
     var_list = [convert_var(var) for var in args]
-    sql = "SELECT " + function_name + "(" + ", ".join(["{}"]*len(var_list)).format(*var_list) + ");"
+    sql = "SELECT " + function_name + "(" + ", ".join(["{}"]*len(var_list)).format(*var_list) + ")"
     return run_sql(sql)
 
 def function_row_type(function_name, dict):
     var_list = [convert_var(value) for key, value in dict.items()]
-    sql = "SELECT " + function_name + "((" + ", ".join(["{}"]*len(var_list)).format(*var_list) + "));"
+    sql = "SELECT " + function_name + "((" + ", ".join(["{}"]*len(var_list)).format(*var_list) + "))"
     return run_sql(sql)
